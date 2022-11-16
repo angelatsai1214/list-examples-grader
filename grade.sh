f48fd0c44f548ac8218a11057808df23b84b30b9
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2022-11-16 14:47:02 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,35 @@
-# Create your grading script here
-
 set -e
+
+CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 git clone $1 student-submission
 
+cd student-submission/
+
+FILE="ListExamples.java"
+
+if [ -f $FILE ]
+then
+        echo "Correct files submitted"
+else
+        echo "Incorrect files submitted"
+        exit 1
+fi
+
+cd ..
+
+cp ListTests.java student-submission/
+echo "GradeTest copied over"
+
+cp -r lib student-submission/
+echo "lib folder copied over"
+
+cd student-submission
+echo "In student-submission"
+
+javac -cp $CP *.java
+echo "Compiled"
+
+java -cp $CP org.junit.runner.JUnitCore  ListTests
+echo "Ran"
