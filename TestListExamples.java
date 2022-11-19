c2643896af97bb2e3d74ce0c866325d9ace7afe4
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2022-11-19 14:51:37 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -51,22 +51,4 @@ public class TestListExamples {
         assertArrayEquals(new String[] {}, liAfterFilter.toArray());
     }
 
-    @Test
-    public void testMerge() {
-        List<String> li1 = new ArrayList<>();
-        li1.add("a");
-        li1.add("c");
-        li1.add("e");
-
-        List<String> li2 = new ArrayList<>();
-        li2.add("b");
-        li2.add("d");
-        li2.add("g");
-        String[] ans = { "a", "b", "c", "d", "e", "g" };
-        List<String> merged = ListExamples.merge(li1, li2);
-
-        assertArrayEquals(ans, merged.toArray());
-
-    }
-
 }
