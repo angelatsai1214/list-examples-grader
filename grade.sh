7d74e431ad49b6bf7aa99389efd8dcff535e8675
Angela Tsai <64194102+angelatsai1214@users.noreply.github.com>
2022-11-19 14:51:04 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,6 @@
-set -e
+# Create your grading script here
+
+# set -e
 
 CP=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 
@@ -9,18 +11,27 @@ cd student-submission/
 
 FILE="ListExamples.java"
 
+# Check if the correct files were submitted
 if [ -f $FILE ]
 then
         echo "Correct files submitted"
 else
-        echo "Incorrect files submitted"
+        echo "Submitted file has the wrong name or path"
         exit 1
 fi
 
-cd ..
+TESTFILE="TestListExamples.java"
 
-cp ListTests.java student-submission/
-echo "GradeTest copied over"
+# Copy test file to student-submission folder if not already exist
+if [[ -s TESTFILE ]]
+then
+        echo "Test file already exist"
+        cd ..
+else
+        cd ..
+        cp TestListExamples.java student-submission/
+        echo "Test file copied to student-submission folder"
+fi
 
 cp -r lib student-submission/
 echo "lib folder copied over"
@@ -28,8 +39,42 @@ echo "lib folder copied over"
 cd student-submission
 echo "In student-submission"
 
-javac -cp $CP *.java
-echo "Compiled"
+javac -cp $CP *.java 2> compiledResults.txt
 
-java -cp $CP org.junit.runner.JUnitCore  TestListExamples
-echo "Ran"
+# Check if there was a compiler error
+ERRORNUMS=$( grep -o 'error' compiledResults.txt | wc -l | xargs )
+if [ $ERRORNUMS -eq 0 ]
+then
+        echo "Compiled"
+else
+        echo "You have COMPILER ERROR"
+        exit 1
+fi
+
+TOTALTESTS=2
+
+# Remove results.txt if it exists already
+if [[ -s results.txt ]]
+then
+        rm results.txt
+fi
+
+java -cp $CP org.junit.runner.JUnitCore TestListExamples > results.txt
+
+echo "Ran Junit Tests"
+
+# Remove testResults.txt if it exists already
+if [[ -s testResults.txt ]]
+then
+        rm testResults.txt
+fi
+
+# sed -n '10,13p' results.txt > testResults.txt
+awk 'NR>=11 && NR<=13' results.txt > testResults.txt
+
+ERROR=$(grep -o 'E' testResults.txt | wc  -l | xargs )
+echo "Number of tests failed: $ERROR"
+CORRECT=$(( $TOTALTESTS-$ERROR ))
+GRADE=$(( ($CORRECT/$TOTALTESTS)*100 ))
+
+echo "Grade: $GRADE"
